707cfb653960e574686531f9f6cf68643ada9394
albaramohammed <albara.mh2015@gmail.com>
2024-05-23 19:27:27 +0300

    Colors

--- CalorieExceededEventArgs.cs
+++ CalorieExceededEventArgs.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PROGPOE
-//__________________________________________Part 2 _____________________________________________
+//_________________________________________Part 2 _______________________________________________
 {
     public class
         CalorieExceededEventArgs : EventArgs
